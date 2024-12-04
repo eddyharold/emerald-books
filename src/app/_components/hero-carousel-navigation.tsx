51e027ebddde682433aa58e5c4e9a6b6dd3f94cc
Harorudo <harorudo.dev@gmail.com>
2024-12-04 21:10:33 +0100

    Replace Swiper with Embla Carousel in hero component; update dependencies in package.json and package-lock.json. Remove unused Swiper code and implement custom navigation buttons for improved functionality.

--- src/app/_components/hero-carousel-navigation.tsx
+++ src/app/_components/hero-carousel-navigation.tsx
@@ -1,31 +1,78 @@
-import { useSwiper } from "swiper/react";
-import { Button } from "../../components/ui/button";
+"use client";
+
+import React, { ComponentPropsWithRef, useCallback, useEffect, useState } from "react";
+import { EmblaCarouselType } from "embla-carousel";
+import { Button } from "@/components/ui/button";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
-export function HeroCarouselNavigationPrev() {
-  const swiper = useSwiper();
+type UsePrevNextButtonsType = {
+  prevBtnDisabled: boolean;
+  nextBtnDisabled: boolean;
+  onPrevButtonClick: () => void;
+  onNextButtonClick: () => void;
+};
 
+export const usePrevNextButtons = (
+  emblaApi: EmblaCarouselType | undefined,
+  onButtonClick?: (emblaApi: EmblaCarouselType) => void
+): UsePrevNextButtonsType => {
+  const [prevBtnDisabled, setPrevBtnDisabled] = useState(true);
+  const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
+
+  const onPrevButtonClick = useCallback(() => {
+    if (!emblaApi) return;
+    emblaApi.scrollPrev();
+    if (onButtonClick) onButtonClick(emblaApi);
+  }, [emblaApi, onButtonClick]);
+
+  const onNextButtonClick = useCallback(() => {
+    if (!emblaApi) return;
+    emblaApi.scrollNext();
+    if (onButtonClick) onButtonClick(emblaApi);
+  }, [emblaApi, onButtonClick]);
+
+  const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+    setPrevBtnDisabled(!emblaApi.canScrollPrev());
+    setNextBtnDisabled(!emblaApi.canScrollNext());
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    onSelect(emblaApi);
+    emblaApi.on("reInit", onSelect).on("select", onSelect);
+  }, [emblaApi, onSelect]);
+
+  return {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick,
+  };
+};
+
+type PropType = Pick<ComponentPropsWithRef<"button">, "onClick" | "disabled">;
+
+export const PrevButton = (props: PropType) => {
   return (
     <Button
       variant="accent"
       className="size-10 rounded-full group opacity-50 hover:opacity-100 transition-opacity duration-150 ease-out"
-      onClick={() => swiper.slidePrev()}
+      {...props}
     >
       <ChevronLeft className="group-hover:scale-150 transition-transform duration-150 ease-in-out" />
     </Button>
   );
-}
+};
 
-export function HeroCarouselNavigationNext() {
-  const swiper = useSwiper();
-
+export const NextButton = (props: PropType) => {
   return (
     <Button
       variant="accent"
       className="size-10 rounded-full group opacity-50 hover:opacity-100 transition-opacity duration-150 ease-out"
-      onClick={() => swiper.slideNext()}
+      {...props}
     >
       <ChevronRight className="group-hover:scale-150 transition-transform duration-150 ease-in-out" />
     </Button>
   );
-}
+};
--- src/app/_components/hero-carousel.tsx
+++ src/app/_components/hero-carousel.tsx
@@ -1,17 +1,15 @@
 "use client";
 
-import React, { useMemo } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectFade, Autoplay } from "swiper/modules";
-
-import { SMOOTH_DARK_COLOR } from "@/core/constants/colors";
-
-import "swiper/css";
-import "swiper/css/effect-fade";
-import { ButtonLink } from "../../components/ui/button";
-import { HeroCarouselNavigationNext, HeroCarouselNavigationPrev } from "./hero-carousel-navigation";
-import { SwiperOptions } from "swiper/types";
+import React, { useCallback, useEffect } from "react";
+import { motion } from "motion/react";
+import useEmblaCarousel from "embla-carousel-react";
+import Fade from "embla-carousel-fade";
+import Autoplay from "embla-carousel-autoplay";
+import { ButtonLink } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import { SMOOTH_DARK_COLOR } from "@/core/constants/colors";
+import { NextButton, PrevButton, usePrevNextButtons } from "./hero-carousel-navigation";
+import { EmblaCarouselType } from "embla-carousel";
 
 const slides = [
   { image: "slide-1.webp", text: "Text for Slide 1" },
@@ -19,82 +17,99 @@ const slides = [
   { image: "slide-3.webp", text: "Text for Slide 3" },
 ];
 
-const HeroCarousel = () => {
-  const options: SwiperOptions = useMemo(
-    () => ({
+const animationVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0 },
+};
+
+const EmblaCarousel = () => {
+  // const [selectedSlide, setSelectedSlide] = useState(0);
+
+  const [emblaRef, emblaApi] = useEmblaCarousel(
+    {
       loop: true,
-      effect: "fade",
-      spaceBetween: 0,
-      slidesPerView: 1,
-      speed: 1200,
-      centeredSlides: true,
-      allowTouchMove: true,
-      virtualTranslate: true,
-      watchOverflow: true,
-      preventInteractionOnTransition: true,
-      autoplay: {
+      duration: 30,
+    },
+    [
+      Fade(),
+      Autoplay({
         delay: 8000,
-        disableOnInteraction: false,
-        waitForTransition: false,
-      },
-      breakpoints: {
-        1024: {
-          allowTouchMove: false,
-        },
-      },
-    }),
-    []
+      }),
+    ]
   );
 
+  const onNavigate = useCallback((emblaApi: EmblaCarouselType | undefined) => {
+    if (emblaApi && emblaApi.slidesInView().length === 1) {
+      // setSelectedSlide(emblaApi.slidesInView()[0]);
+    }
+  }, []);
+
+  const { prevBtnDisabled, nextBtnDisabled, onPrevButtonClick, onNextButtonClick } = usePrevNextButtons(emblaApi);
+
+  useEffect(() => {
+    if (emblaApi) emblaApi.on("slidesInView", onNavigate);
+  }, [emblaApi, onNavigate]);
+
   return (
-    <Swiper {...options} modules={[EffectFade, Autoplay]} className="h-[60vh] lg:h-[80vh] relative">
-      {slides.map((slide, index) => (
-        <SwiperSlide key={`splide-slide-${index}`}>
-          <div
-            className="w-full h-full bg-cover bg-center"
-            style={{
-              backgroundImage: `
-                linear-gradient(
-                  rgba(${SMOOTH_DARK_COLOR.RGB}, 0.5),
-                  rgba(${SMOOTH_DARK_COLOR.RGB}, 0.6),
-                  rgba(${SMOOTH_DARK_COLOR.RGB}, 0.7)
-                ),
-                url('/assets/images/hero-slides/${slide.image}')
-              `,
-            }}
-          >
-            <div className="h-full flex flex-col justify-center container text-muted text-center">
-              <h1 className="text-4xl md:text-5xl lg:text-7xl md:max-w-5xl mx-auto font-bold mb-4 font-recoleta">
-                Empowering Education with <span className="text-brand-200">Quality Publishing</span>
-              </h1>
+    <div className="embla">
+      <div className="embla__viewport overflow-hidden h-[60vh] lg:h-[80vh] relative" ref={emblaRef}>
+        <div className="embla__container flex w-full h-full">
+          {slides.map((slide, index) => (
+            <div
+              key={`splide-slide-${index}`}
+              className="embla__slide w-full h-full bg-cover bg-center flex-[0_0_100%] min-w-0"
+              style={{
+                backgroundImage: `
+                    linear-gradient(
+                        rgba(${SMOOTH_DARK_COLOR.RGB}, 0.7),
+                        rgba(${SMOOTH_DARK_COLOR.RGB}, 0.7)
+                    ),
+                    url('/assets/images/hero-slides/${slide.image}')
+                `,
+              }}
+            >
+              <motion.div
+                // key={`slide-${selectedSlide}-${selectedSlide === index ? "active" : "standby"}`}
+                initial="hidden"
+                animate="visible"
+                exit="hidden"
+                variants={animationVariants}
+                transition={{ duration: 0.5, ease: "easeInOut" }}
+                className="h-full flex flex-col justify-center container text-muted text-center"
+              >
+                <h1 className="text-4xl md:text-5xl lg:text-7xl md:max-w-5xl mx-auto font-bold mb-4 font-recoleta">
+                  Empowering Education with <span className="text-brand-200">Quality Publishing</span>
+                </h1>
 
-              <p className="md:text-xl lg:text-2xl md:max-w-2xl mx-auto mb-8">
-                Discover educational resources that shape the future of learners in Cameroon and Central Africa. Our
-                commitment to quality ensures that every book we publish meets the highest standards of excellence.
-              </p>
+                <p className="md:text-xl lg:text-2xl md:max-w-2xl mx-auto mb-8">
+                  Discover educational resources that shape the future of learners in Cameroon and Central Africa. Our
+                  commitment to quality ensures that every book we publish meets the highest standards of excellence.
+                </p>
 
-              <div className="space-x-4">
-                <ButtonLink
-                  href="/books"
-                  className="group rounded-full hover:scale-110 transition-transform duration-150 ease-in-out h-10 md:h-11 lg:h-12 min-w-[18rem] md:text-base lg:text-lg"
-                >
-                  Explore our collections
-                  <ArrowRight />
-                </ButtonLink>
-              </div>
+                <div className="space-x-4">
+                  <ButtonLink
+                    href="/books"
+                    className="group rounded-full hover:scale-110 transition-transform duration-150 ease-in-out h-10 md:h-11 lg:h-12 min-w-[18rem] md:text-base lg:text-lg"
+                  >
+                    Explore our collections
+                    <ArrowRight />
+                  </ButtonLink>
+                </div>
+              </motion.div>
             </div>
-          </div>
-        </SwiperSlide>
-      ))}
+          ))}
+          {/* </div> */}
+        </div>
 
-      <div className="absolute top-0 left-6 z-10 h-full hidden lg:flex items-center">
-        <HeroCarouselNavigationPrev />
+        <div className="absolute top-0 left-6 z-10 h-full hidden lg:flex items-center">
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+        </div>
+        <div className="absolute top-0 right-6 z-10 h-full hidden lg:flex items-center">
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+        </div>
       </div>
-      <div className="absolute top-0 right-6 z-10 h-full hidden lg:flex items-center">
-        <HeroCarouselNavigationNext />
-      </div>
-    </Swiper>
+    </div>
   );
 };
 
-export default HeroCarousel;
+export default EmblaCarousel;
